5a94018ae92b68b989f24534268b2185f05efba4
Shahadat Hossain <shahadathossain@sam.solutions>
2019-07-08 12:28:53 +0600

    onclick respons on Driver

--- app/src/main/java/com/samsolution/bottomsheetexample/MainActivity.java
+++ app/src/main/java/com/samsolution/bottomsheetexample/MainActivity.java
@@ -6,10 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private LinearLayout bottomsheetlayout;
+    private LinearLayout bottomsheetlayout, driverLayout;
     private BottomSheetBehavior bottomSheetBehavior;
 
     ImageView imgshare;
@@ -20,11 +21,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bottomsheetlayout =  findViewById(R.id.bottomSheet);
+        driverLayout =  findViewById(R.id.driverLayout);
         imgshare = findViewById(R.id.shareimg);
         bottomSheetBehavior = BottomSheetBehavior.from(bottomsheetlayout);
 
         //STATE_HIDDEN
         bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+        
         //STATE_COLLAPSED
         //bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
         //STATE_EXPANDED
@@ -38,5 +41,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        driverLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Clicked on Driver Layout", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
     }
 }
