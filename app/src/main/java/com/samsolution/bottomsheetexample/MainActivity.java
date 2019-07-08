84e6ab46cd2eaf68517e9c3499d9c17fa228992b
Shahadat Hossain <shahadathossain@sam.solutions>
2019-07-08 12:49:07 +0600

    ListView With onClick is Added

--- app/src/main/java/com/samsolution/bottomsheetexample/MainActivity.java
+++ app/src/main/java/com/samsolution/bottomsheetexample/MainActivity.java
@@ -1,33 +1,45 @@
 package com.samsolution.bottomsheetexample;
 
+import android.graphics.Color;
+import android.os.Bundle;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private LinearLayout bottomsheetlayout, driverLayout;
-    private BottomSheetBehavior bottomSheetBehavior;
+    private BottomSheetBehavior<LinearLayout> bottomSheetBehavior;
 
-    ImageView imgshare;
+    ListView countryListLV;
+
+    String[] countryListData = {"Bangladesh", "India", "Pakistan", "Dhaka", "Khulna", "Bhola", "Bangladesh", "India", "Pakistan", "Dhaka", "Khulna", "Bhola", "Bangladesh", "India", "Pakistan", "Dhaka", "Khulna", "Bhola", "Bangladesh", "India", "Pakistan", "Dhaka", "Khulna", "Bhola", "Bangladesh", "India", "Pakistan", "Dhaka", "Khulna", "Bhola", "Bangladesh", "India", "Pakistan", "Dhaka", "Khulna", "Bhola"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomsheetlayout =  findViewById(R.id.bottomSheet);
-        driverLayout =  findViewById(R.id.driverLayout);
-        imgshare = findViewById(R.id.shareimg);
+        LinearLayout bottomsheetlayout = findViewById(R.id.bottomSheet);
+        //LinearLayout driverLayout = findViewById(R.id.driverLayout);
+
+        countryListLV = findViewById(R.id.countryList);
+
+        ImageView imgshare = findViewById(R.id.shareimg);
         bottomSheetBehavior = BottomSheetBehavior.from(bottomsheetlayout);
 
         //STATE_HIDDEN
         bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
-        
+
+        countryListLV.setBackgroundColor(Color.WHITE);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, countryListData);
+        countryListLV.setAdapter(adapter);
         //STATE_COLLAPSED
         //bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
         //STATE_EXPANDED
@@ -41,12 +53,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        driverLayout.setOnClickListener(new View.OnClickListener() {
+        countryListLV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Clicked on Driver Layout", Toast.LENGTH_SHORT).show();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, " " + countryListData[position], Toast.LENGTH_SHORT).show();
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
             }
         });
-        
     }
 }
